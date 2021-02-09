97c1a2912ec3b4f701714f4fd70cf1aafdf3d3c9
royalrz <rzaxanovroyal1995@gmail.com>
2021-02-09 23:52:07 +0400

    added readme file

--- setup.py
+++ setup.py
@@ -1,16 +1,20 @@
 import setuptools
 
-with open("README.md", "r") as fh:
-    description = fh.read()
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setuptools.setup(
     name = 'django-network-conditions',
-    version = '1.0.2',
+    version = '1.1.1',
     packages = ['django_network_conditions'],
     url = 'http://pypi.python.org/pypi/django-network-conditions',
     license = 'LICENSE',
-    description = description,
-
+    description='Delay the response to simulate the real network conditions',
+    long_description = long_description,
+    long_description_content_type='text/markdown',
     include_package_data=True,
     zip_safe=False,
     install_requires=["scipy"],
--- test_network_condition_simulator/settings.py
+++ test_network_condition_simulator/settings.py
@@ -127,5 +127,5 @@ NETWORK_CONDITIONS = {
     'KB_PER_SECOND': 1024, # speed to send the response,
     'JITTER': 10, # millis standard deviation in latency. 0 = no jitter otherwise gaussian,
     'TIMEOUT_PCT': 5, #the percentage of requests that time out
-    'PRINT_LOGS': False
+    'PRINT_LOGS': False # if True, it shows the status.
 }
